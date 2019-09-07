3bc7097797a501d58ac817eafa04f6fb7fc0e9a4
George <The.Pancake.Master.777@gmail.com>
2019-09-07 17:32:35 +0800

    Susie is a Strawberry

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -2,6 +2,6 @@ public class HelloWorld1
 {
 	public static void main(String[]args)
 	{
-		System.out.println("HelloWorld"); 
+		System.out.println("Susie eats uncooked potatoes while thinking about how the the oceanes fought over a cookie"); 
 	}
 }
