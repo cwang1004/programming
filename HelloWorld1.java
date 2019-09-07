56c2a30e72e52c2703656bffd0952d9b8b1a3c6d
cwang1004 <masteracct.cwang@gmail.com>
2019-09-07 17:45:03 +0800

    Delete HelloWorld1.java

--- HelloWorld1.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld1
-{
-	public static void main(String[]args)
-	{
-		System.out.println("Susie eats uncooked potatoes while thinking about how the the oceanes fought over a cookie"); 
-	}
-}
