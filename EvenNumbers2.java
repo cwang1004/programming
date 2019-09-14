c521d2cd5433d0bffed1d6e4aed9f87a0d1cdb54
susie <2027swang@biba-student.org>
2019-09-14 19:36:03 +0800

    I did my homework before george yay

---
+++ EvenNumbers2.java
@@ -0,0 +1,12 @@
+public class loops2
+{
+	public static void main(String[] args)
+	{
+		int cookies = 0;
+		for(int a = 1; a < 57; a+=1)
+		{
+			System.out.println(cookies);
+			cookies+=2;
+		}
+	}
+}
