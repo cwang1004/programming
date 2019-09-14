009718cdd37798b1e007ee5de78de99fa80f94cc
susie <2027swang@biba-student.org>
2019-09-14 16:58:58 +0800

    susie's stuff

---
+++ Swap2.java
@@ -0,0 +1,13 @@
+public class Swap2
+{
+	public static void main (String[] args)
+	{
+		int a = 1;
+		int b = 2;
+		int c = a;//store a into c. c=1
+		a = b;//assigning a into b. b=1
+		System.out.println(a);
+		b = c;//assigning b into c. b=1
+		System.out.println(b);
+	}
+}
---
+++ Variable2.java
@@ -0,0 +1,10 @@
+public class Variable2
+{
+	public static void main(String[] args)
+	{
+		String name = "Susie";
+		int  age = 10;
+		System.out.println("My name is " + name + " I am " + age + " years old.");
+
+	}
+}
