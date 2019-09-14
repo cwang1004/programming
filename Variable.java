0f14cadb3c5081ce8cf98aac1a66c72b7f85809d
George <The.Pancake.Master.777@gmail.com>
2019-09-14 10:49:31 +0800

    All about a starfish

---
+++ Variable.java
@@ -0,0 +1,10 @@
+public class Variable
+{
+	public static void main(String[] args)
+	{
+		String name = "George";
+		int  age = 2;
+		System.out.println("My name is " + name + ", I am " + age + " years old.");
+
+	}
+}
