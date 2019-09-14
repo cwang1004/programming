db3eaa19e04fbeff8c4b4815b290928bf16df45e
George <The.Pancake.Master.777@gmail.com>
2019-09-14 09:28:43 +0800

    CORRECT homework from George

--- PvsPLN1.java
+++ PvsPLN1.java
@@ -1,8 +1,10 @@
-public class PvsPLN1
+public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello");
+		System.out.println("HelloWorld");
 		System.out.print("Hello");
+		System.out.println("Mushroom");
+		System.out.print("Muffin");
 	}
 }
