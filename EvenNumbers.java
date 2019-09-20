895f91a1e87e978b6776bf7ce02ba052623609ac
Charles <masteracct.cwang@gmail.com>
2019-09-20 20:06:12 +0800

    commented on Starfish's hw

--- EvenNumbers.java
+++ EvenNumbers.java
@@ -2,14 +2,14 @@ public class EvenNumbers
 {
 	public static void main(String[] args)
 	{
-	int taco = 0;
-	for(int sauce = 0; sauce < 110; sauce++)
+	int taco = 0; //indent this line
+	for(int sauce = 0; sauce < 110; sauce++) //and indent this for loop block
 	{
 		System.out.println(taco);
-		taco+=2;
+		taco+=2; //spaces on both sides of '+='
 	}
-}
+} //uhhh, indent this bracket.
 		
 	
 
-}
+} //this one matches with the first bracket. So this is correct.
