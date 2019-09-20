ba0163c58eba1b92c81c94683dffb479a8a27b42
George <The.Pancake.Master.777@gmail.com>
2019-09-20 19:56:27 +0800

    Homework from george

---
+++ EvenNumbers.java
@@ -0,0 +1,15 @@
+public class EvenNumbers
+{
+	public static void main(String[] args)
+	{
+	int taco = 0;
+	for(int sauce = 0; sauce < 110; sauce++)
+	{
+		System.out.println(taco);
+		taco+=2;
+	}
+}
+		
+	
+
+}
