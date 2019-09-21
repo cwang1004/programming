d549c8ad1175ab43960a52f0ab756e36e4f911a4
George <The.Pancake.Master.777@gmail.com>
2019-09-21 11:29:59 +0800

    HW from George

--- EvenNumbers.java
+++ EvenNumbers.java
@@ -3,13 +3,10 @@ public class EvenNumbers
 	public static void main(String[] args)
 	{
 	int taco = 0; //indent this line
-	for(int sauce = 0; sauce < 110; sauce++) //and indent this for loop block
-	{
-		System.out.println(taco);
-		taco+=2; //spaces on both sides of '+='
-	}
-} //uhhh, indent this bracket.
-		
-	
-
+		for(int sauce = 0; sauce < 110; sauce++) //and indent this for loop block
+		{
+			System.out.println(taco);
+			taco += 2; //spaces on both sides of '+='
+		}
+	} //uhhh, indent this bracket.
 } //this one matches with the first bracket. So this is correct.
