958bc7f521baeb9f1c5634f3cea024fd1d5c795e
Charles <masteracct.cwang@gmail.com>
2019-09-22 13:38:14 +0800

    marked susie's hw

--- Funnnnction2.java
+++ Funnnnction2.java
@@ -1,3 +1,23 @@
+/*
+	First part
+
+	public means that it's something others can see and it's loacal
+	private means that it's for your-self,only you can see
+	static's oppisite is nothing
+	return types -- int,String
+	void means return nothing
+	parameters go inside parethesis
+	P.S:Susie is awesome
+	and i reserched. 
+	float is int but can have 6 numbers after the decimal point. 
+	likethis: '1.234567'
+	I guess so. I put the pic in the folder.
+
+	Great work again! I was hoping at least one of you would research that.
+	So float just means decimal numbers. It's not int anymore because ints are only whole numbers with no decimal points.
+	But yes, the example you gave (1.234567) was of type float.
+*/
+
 public class Funnnction2
 {
 	public static void main(String[] args)
@@ -5,23 +25,11 @@ public class Funnnction2
 		System.out.println(cookie(3, 8, 4));
 	}
 
+	/*
+		awesome. completely correct.
+	*/
 	public static float cookie(int acorn, int nut, int pinecone)
 	{
 		return acorn * nut / pinecone;
 	}
 }
-/*
-	First part
-
-	public means that it's something others can see and it's loacal
-	praivate means that it's for your-self,only you can see
-	static's oppisite is nothing
-	return types -- int,String
-	void means return nothing
-	parameters go inside parethesis
-	P.S:Susie is awesome
-	and i reserched.
-	float is int but can have 6 numbers after the decimal point.
-	likethis: '1.234567'
-	I guess so. I put the pic in the folder.
-*/
