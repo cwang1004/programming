c39757e8688ea23703b1a95eebc53aebbe865fa6
susie <2027swang@biba-student.org>
2019-09-22 11:45:33 +0800

    I did my home work

---
+++ Funnnnction2.java
@@ -0,0 +1,27 @@
+public class Funnnction2
+{
+	public static void main(String[] args)
+	{
+		System.out.println(cookie(3, 8, 4));
+	}
+
+	public static float cookie(int acorn, int nut, int pinecone)
+	{
+		return acorn * nut / pinecone;
+	}
+}
+/*
+	First part
+
+	public means that it's something others can see and it's loacal
+	praivate means that it's for your-self,only you can see
+	static's oppisite is nothing
+	return types -- int,String
+	void means return nothing
+	parameters go inside parethesis
+	P.S:Susie is awesome
+	and i reserched.
+	float is int but can have 6 numbers after the decimal point.
+	likethis: '1.234567'
+	I guess so. I put the pic in the folder.
+*/
