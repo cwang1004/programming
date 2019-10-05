952a2c68b328f9d7c1e407dbe314f61019d63bf8
George <The.Pancake.Master.777@gmail.com>
2019-10-05 18:17:53 +0800

    cupcake

--- RPS.java
+++ RPS.java
@@ -1,22 +1,74 @@
+import java.util.*;
 public class RPS
 {
 	public static void main(String[] args)
 	{
-		printRock();
+	//	String eat = "chomp";
+	//	int susie = 0;
+	//	float pie = 3.14;
+		Scanner input = new Scanner(System.in);
+
+		while(true)
+		{
+		int option = 0;
+		while (option > 3 || option < 1)
+		{
+			System.out.println("Enter your option: Type 1 for Rock, Type 2 for Paper or Type 3 for Scissor.");
+			option = input.nextInt();
+		}
+
+		if(option == 1)
+		{
+			printRock();
+		}
+		else if(option == 2)
+		{
+			printScissor();
+		}
+		else
+		{
+			printPaper();
+		}
 	}
+}
 
 	public static void printRock()
 	{
 		System.out.println("");
 		System.out.println("  I'm A Rock");
 		System.out.println("  *********");
-		System.out.println(" * *    *  *");
+		System.out.println(" * O     O *");
 		System.out.println(" *         *");
 		System.out.println(" * *     * *");
 		System.out.println(" *  * * *  *");
 		System.out.println("  *********");
 		System.out.println("");
-		System.out.println("");
-		System.out.println("");
+	}
+
+	public static void printScissor()
+	{
+		System.out.println(" **    ** ");
+		System.out.println(" * *  * * ");
+		System.out.println("  * ** *  ");
+		System.out.println("   *OO*   ");
+		System.out.println("   ****   ");
+		System.out.println("  **--**  ");
+		System.out.println(" ***  *** ");
+		System.out.println(" * *  * * ");
+		System.out.println(" **    ** ");
+		System.out.println("          ");
+	}
+	public static void printPaper()
+	{
+		System.out.println("   * * *");
+		System.out.println("  * * * *");
+		System.out.println(" ** * * *");
+		System.out.println("* *o* *o*");
+		System.out.println("* * * * *");
+		System.out.println("*  ---- *");
+		System.out.println(" *     * ");
+		System.out.println("   *  *");
+		System.out.println("   *  *");
+		System.out.println("   *  *");
 	}
 }
