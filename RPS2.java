55b3a453bb8891f4518b96326d134b9d3390f36c
susie <2027swang@biba-student.org>
2019-10-05 16:53:43 +0800

    Susie don't like george

---
+++ RPS2.java
@@ -0,0 +1,21 @@
+public class RPS2
+{
+	public static void main(String[] args)
+	{
+		printScissor();
+	}
+
+	public static void printScissor()
+	{
+		System.out.println(" **    ** ");
+		System.out.println(" * *  * * ");
+		System.out.println("  * ** *  ");
+		System.out.println("   *OO*   ");
+		System.out.println("   ****   ");
+		System.out.println("  **--**  ");
+		System.out.println(" ***  *** ");
+		System.out.println(" * *  * * ");
+		System.out.println(" **    ** ");
+		System.out.println("          ");
+	}
+}
