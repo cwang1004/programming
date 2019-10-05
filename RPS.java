68efc48e8bb17e2ae412fc149e70e4cb5e833dc4
George <The.Pancake.Master.777@gmail.com>
2019-10-05 16:52:22 +0800

    George's taco

---
+++ RPS.java
@@ -0,0 +1,22 @@
+public class RPS
+{
+	public static void main(String[] args)
+	{
+		printRock();
+	}
+
+	public static void printRock()
+	{
+		System.out.println("");
+		System.out.println("  I'm A Rock");
+		System.out.println("  *********");
+		System.out.println(" * *    *  *");
+		System.out.println(" *         *");
+		System.out.println(" * *     * *");
+		System.out.println(" *  * * *  *");
+		System.out.println("  *********");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+	}
+}
