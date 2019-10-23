c2c7fc757d51a7a820eaee821f69b514927ad37f
Neil <neil20091220@163.com>
2019-10-23 19:37:18 +0800

    sync

---
+++ StudentPorfile3.java
@@ -0,0 +1,33 @@
+//neil's homrwork!!!!!!!!!!!!!
+
+
+
+import java.util.*;
+
+public class StudentPorfile3
+{
+	public static void main(String[] args)
+	{
+		System.out.println("enter your username");
+		name = input.nextstring();
+		System.out.println("enter your age");
+		age = input.nextint();
+		System.out.println("enter your gender 1-male 2-female");
+		gender = input.nextint();
+		while(gende < 1 || > 2)
+		if(gender == 1 )
+		gender = input.nextdouble();
+		else 
+		gender = input.nextdouble();
+		System.out.println("enter your hight");
+		hight = input.nextstring();
+		System.out.println("loading..."); 
+		System.out.println("loading...");
+		System.out.println("loading...");
+		System.out.println("loading...");
+		System.out.println("loading...");
+		System.out.println("name,age,gender,hight");
+
+
+	}
+}
