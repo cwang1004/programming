1bfa1383ab4b18a9798476718eb061b2e71bd7e7
George <The.Pancake.Master.777@gmail.com>
2019-10-24 19:10:27 +0800

    Question from George

---
+++ StudentProfile.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class StudentProfile
+{
+	public static void main(String[] args)
+	{
+		Scanner name = new Scanner(System.in);
+		System.out.println("What is your name? Type 0 for susie, 1 for George, 2 for mike or 3 for neil.");
+		name = name.nextInt();
+	}
+}
+/*
+Charles where do you store the anwser?
+*/
