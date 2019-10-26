169123be7d34f4eda6b46fb73a84be110941ba4a
George <The.Pancake.Master.777@gmail.com>
2019-10-26 19:17:56 +0800

    FINISHED

--- StudentProfile.java
+++ StudentProfile.java
@@ -3,11 +3,39 @@ public class StudentProfile
 {
 	public static void main(String[] args)
 	{
-		Scanner name = new Scanner(System.in);
-		System.out.println("What is your name? Type 0 for susie, 1 for George, 2 for mike or 3 for neil.");
-		name = name.nextInt();
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("What is your name?");
+		String name = input.nextLine();
+		input = new Scanner(System.in);
+		int gender = 0;
+		while (gender > 2 || gender < 1)
+		{
+			System.out.println("What is your gender? 1-male, 2-female.");
+			gender = input.nextInt();
+		}
+		Double height = 0.00;
+		System.out.println("Enter your height in centimeters.");
+		height = input.nextDouble();
+		int age = 0;
+		System.out.println("Enter your age.");
+		age = input.nextInt();
+		System.out.println("Name: " + name);
+		if(gender == 1)
+		{
+			System.out.println("Gender: Male");
+		}
+		else
+		{
+			System.out.println("Gender: Female");
+		}
+		System.out.println("Height: " + height);
+		System.out.println("Age: " + age);
+		System.out.println("Creation Successfull!");
+
+
 	}
 }
 /*
-Charles where do you store the anwser?
+'-'
 */
