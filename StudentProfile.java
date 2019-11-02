5370a6aac74687652912595133e32f98938fdfc8
Charles <masteracct.cwang@gmail.com>
2019-11-02 16:24:55 +0800

    fixed George's hw

--- StudentProfile.java
+++ StudentProfile.java
@@ -7,19 +7,22 @@ public class StudentProfile
 
 		System.out.println("What is your name?");
 		String name = input.nextLine();
-		input = new Scanner(System.in);
+		
 		int gender = 0;
 		while (gender > 2 || gender < 1)
 		{
 			System.out.println("What is your gender? 1-male, 2-female.");
 			gender = input.nextInt();
 		}
+		
 		Double height = 0.00;
 		System.out.println("Enter your height in centimeters.");
 		height = input.nextDouble();
+		
 		int age = 0;
 		System.out.println("Enter your age.");
 		age = input.nextInt();
+		
 		System.out.println("Name: " + name);
 		if(gender == 1)
 		{
@@ -32,8 +35,6 @@ public class StudentProfile
 		System.out.println("Height: " + height);
 		System.out.println("Age: " + age);
 		System.out.println("Creation Successfull!");
-
-
 	}
 }
 /*
