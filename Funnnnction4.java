98357ab64a91150d0cfdf45d250cecbb5e9a06ef
Mike <mike20091016@163.com>
2019-11-02 16:17:45 +0800

    catchup4.java

---
+++ Funnnnction4.java
@@ -0,0 +1,11 @@
+/*
+1.public means everyone can use it
+2.private means only one person can use it 
+3.nothing
+4.Scanner String int float double String[]
+5.return nothing 
+6.inside (    )
+*/
+
+
+mike is awesome!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
