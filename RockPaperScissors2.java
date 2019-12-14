92b294290fee756b13bc2e930eb3f5e4357ec741
susie <2027swang@biba-student.org>
2019-12-14 13:42:43 +0800

    new files

--- RockPaperScissors2.java
+++ RockPaperScissors2.java
@@ -1,4 +1,4 @@
-import java.util.*;
+	import java.util.*;
 public class RockPaperScissors
 {
 	public static void main(String[] args)
--- StudentProfile2.java
+++ StudentProfile2.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class StudentProfile22
+public class StudentProfile2
 {
 	public static void main(String[] args)
 	{
