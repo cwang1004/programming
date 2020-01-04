8cdb00e291c512d732866d47909bc73a638293b6
Mike <mike20091016@163.com>
2020-01-04 13:46:27 +0800

    sync

--- RockPaperScissors4.java
+++ RockPaperScissors4.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class RockPaperScissors2
+public class RockPaperScissors4   
 {
 	public static void main(String[] args)
 	{
@@ -9,18 +9,60 @@ public class RockPaperScissors2
 		while(playerOption < 1 || playerOption > 3)
 		{
 			System.out.println("Enter your option: 1=Rock, 2=Paper and 3=Scissor");	
-			option = input.nextInt();
+			playerOption = input.nextInt();
 		}
 
 		Random randomizer = new Random();
 		int computerOption = randomizer.nextInt(3) + 1;
-		int i = 0;
-		while(i < 10)
+		showOption(playerOption, computerOption);
+		printResults();
+	}
+
+	public static void showOptions(int player, int computer)
+	{
+		System.out.println("player")
+		if(player==1)
 		{
-			System.out.println(computerOption);
-			computerOption = randomizer.nextInt(3) + 1;
-			i++;
+			printRock();
 		}
+		else if(player==2)
+		{
+			printPaper();
+		}
+		else(player==3)
+		{
+			printScissor();
+		}
+
+		System.out.println("computer")
+		if(computer==1)
+		{
+			printRock();
+		}
+		else if(computer==2)
+		{
+			printPaper();
+		}
+		else(coputer==3)
+		{
+			printScissor();
+		}
+	}
+	public static void printResults(int player, int computer)
+	{
+		int result = computer - player;
+	    if( result == 1 || result == -2)
+		{
+			System.out.println("you lost ");
+		}
+		else if(result == 0)
+	 	{
+	 		System.out.println("draw");
+	 	}
+	 	else
+	 	{
+	 		System.out.println("you win");
+	 	}
 	}
 
 	public static void printRock()
@@ -64,4 +106,3 @@ public class RockPaperScissors2
 		System.out.println("   *  *");
 	}
 }
-
