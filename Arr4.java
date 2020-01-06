57993d74f5f0f0a4a13d0a210203ba6669f08132
Mike <mike20091016@163.com>
2020-01-06 21:51:02 +0800

    Arr4.java

---
+++ Arr4.java
@@ -0,0 +1,23 @@
+public class Arr4
+{
+	public static void main(String[] args)
+	{
+		String [] arr= new String [4];//if you do not know what the array is 
+		String [] names= {"George,Susie,Neil,Mike"};//if you have a idea of what the array is about
+		arr[4];//the number of buckets is in the []
+		System.out.println(arr[4]);
+		int [] arr=new int [100];//creating a array of 100 buckets
+		for (int i=0; <49; i++ );//start from 0 to 49
+		{
+			arr[i]=1//im not sure what is this part 
+			System.out.println(arr[]);//print from 0 to 49
+		}
+	}	
+	public static int get(int[]arr,int index);//creating two type int ,name arr and index varieble 
+	{
+		return arr[index];//return information
+		System.out.println(get);//print information
+	}
+		
+} 
+//im not sure if this code is right.can you correct it if its wrong? thanks Mr.Charles.	
