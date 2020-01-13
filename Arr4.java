724b696469f0617ef7ac8c1c4b9f56766f826263
Mike <mike20091016@163.com>
2020-01-13 12:47:22 +0800

    Arr4.java

--- Arr4.java
+++ Arr4.java
@@ -12,13 +12,38 @@ public class Arr4
 			arr[i]=1//im not sure what is this part 
 			System.out.println(arr[]);//print from 0 to 49
 		}
-	}	
+		int[] neil=new int[10];//creating a array name Neil with 11 buckets( including 0)
+		for (int you=0; you<10; you+=2 )//if "you" is less than 10 , then +2 to go to the next number ( starts from 0) 
+		{
+			neil[you] =you;//print the bucket's number .
+		}
+	}																																						
 	public static int get(int[]arr,int index);//creating two type int ,name arr and index varieble 
 	{
 		return arr[index];//return information
 		System.out.println(get);//print information
+	}//the code under is similiar to flipping cards , so think on a different way
+	public static int currentMax(int[] arr) //creating a public , static int varieble name currentMax. ( and a type int varieble name arr) 
+	{
+		int currentMax = arr[0];//the system now knew the first "card" so it think 0 is the biggest
+		for(int i=1; i<arr.length;i++)// if i is less than arr's length ( which means how many buckets ),keep "flipping the next card"
+		{
+			if(arr[i]>currentMax);//if "the card flipped just now" is more than the currentMax known before , ↓
+			{
+				currentMax = arr[i];//then make it the currentMax
+			}
+		}
+		return currentMax;//store currentMax 
 	}
+	/*public static int currentMin(int[] arr)
+	{
+		int currentMin = arr[0];
+		for(int a=1; i<arr.length;i++)
+		{
+			if(arr[i]>)
+		}
+	}
+	*/
+	//unfinished program ↑
 		
 } 
-//im not sure if this code is right.can you correct it if its wrong? thanks Mr.Charles.
-//mike2020/1/7:what is code the classic Mr.Charles???
