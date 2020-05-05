0eaa2ab9f49980dc56c9d1448f86c2979bb5a424
susie <2027swang@biba-student.org>
2020-05-05 15:48:34 +0200

    popo face from susie

---
+++ HelloWorld2.java
@@ -0,0 +1,15 @@
+public class HelloWorld2
+{
+	public static void main(String[] args)
+	{
+		String meal = "";
+		String breakfeast = "cereal,";
+		String lunch = " noooodles,";
+		String dinner = " and soup";
+		meal += breakfeast;
+		meal += lunch;
+		meal += dinner;
+		System.out.println(meal);
+		/*homework: create variable called meal, store breakfeast, lunch, and dinner*/
+	}
+}
